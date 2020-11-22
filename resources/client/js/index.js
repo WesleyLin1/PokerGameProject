44bb52f254b689e73164914902099de2a469a6b1
WesleyLin1 <92919@farnborough.ac.uk>
2020-11-22 21:00:26 +0000

    - Added index.js, and moved functions to login.js and index.js - Renamed welcome.html to index.html - Renamed index.html to misc.html

---
+++ resources/client/js/index.js
@@ -0,0 +1,20 @@
+// Logout function
+function logout() {
+    debugger;
+    console.log("Invoked logout");
+    let url = "/users/logout";
+    fetch(url, {method: "POST"
+    }).then(response => {
+        return response.json();                 //now return that promise to JSON
+    }).then(response => {
+        if (response.hasOwnProperty("Error")) {
+            alert(JSON.stringify(response));        // if it does, convert JSON object to string and alert
+        } else {
+            Cookies.remove("token", response.token);    //UserName and Token are removed
+            Cookies.remove("name", response.name);
+            window.open("login.html", "_self");       //open misc.html in same tab
+        }
+    });
+    // test
+}
+// Tutorial redirect button
---
+++ resources/client/js/login.js
@@ -0,0 +1,24 @@
+// Login func
+function usersLogin() {
+    //debugger;
+    console.log("Invoked UsersLogin() ");
+    let url = "/users/login";
+    let formData = new FormData(document.getElementById('LoginForm'));
+
+    fetch(url, {
+        method: "POST",
+        body: formData,
+    }).then(response => {
+        return response.json();                 //now return that promise to JSON
+    }).then(response => {
+        if (response.hasOwnProperty("Error")) {
+            alert(JSON.stringify(response));        // if it does, convert JSON object to string and alert
+        } else {
+            debugger;
+            console.log(response.token + " " + response.name);
+            Cookies.set("token", response.token);
+            Cookies.set("name", response.name);
+            window.open("index.html", "_self");       //open misc.html in same tab
+        }
+    });
+}
