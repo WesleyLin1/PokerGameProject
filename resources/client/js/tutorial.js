5624f6405b857e5bd27da0c65a7541c12785f7ca
WesleyLin1 <92919@farnborough.ac.uk>
2020-11-24 21:04:27 +0000

    - Removed cards in the clients file - Added changes to tutorial.html and tutorial.js

--- resources/client/js/tutorial.js
+++ resources/client/js/tutorial.js
@@ -7,19 +7,16 @@ It is zero-based so I can utilise iterative formulas to generate a deck arrangem
 let spades = [0,1,2,3,4,5,6,7,8,9,10,11,12];
 let clubs = [13,14,15,16,17,18,19,20,21,22,23,24,25];
 let diamonds = [26,27,28,29,30,31,32,33,34,35,36,37,38];
-let hearts = [39,40,41,42,43,44,45,46,47,48,49,50,51]
-let deck = spades.concat(clubs,diamonds,hearts)
+let hearts = [39,40,41,42,43,44,45,46,47,48,49,50,51];
+let deck = spades.concat(clubs,diamonds,hearts);
 console.log(deck);
 
 // Adds images of cards
-/* I'm just going to dump the card images in the current folder until I figure out what's wrong with the file path
-I've spent a good 3 hours staring at this code and I can't figure out why the file path doesn't work, so I'm gonna do it the caveman way
-*/
-function addimg(){
+function addimg() {
     for (let i=0; i<deck.length; i++){
         debugger;
         let x = document.createElement("img");
-        x.setAttribute("src",i+".png" ); // Ideally this should be the picture path, but path references don't seem to function properly, so in the bin it goes
+        x.setAttribute("src","/client/img/" + i +".png" );
         x.setAttribute("width", "65");
         x.setAttribute("height", "100");
         x.setAttribute("alt", "card");
