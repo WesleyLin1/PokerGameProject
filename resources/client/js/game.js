26bca74fd8e4c88bea78324c255fdc52dce71097
WesleyLin1 <92919@farnborough.ac.uk>
2020-12-10 22:21:47 +0000

    - Slight modifications to source code with deck shuffling and display

--- resources/client/js/game.js
+++ resources/client/js/game.js
@@ -6,17 +6,22 @@ Array.prototype.swap = function (x,y) {
     return this;
 };
 
+/*
+These variables will be used throughout when describing the addition or transformation of elements on the canvas
+ */
+let canvas = document.getElementById("cardCanvas");
+let context = canvas.getContext("2d");
+
 //-----------------------------
 // Deck Generation and Shuffle
 //-----------------------------
 
 let deck = []; // Declare empty array deck
 let cardsLoaded = 0; // Sets the amount of cards loaded to 0
-const suitNames = ["Spades", "Clubs", "Diamond", "Hearts"];
-const valueNames = ["Ace", "2","3","4","5","6","7","8","9","10","Jack", "Queen", "King"];
+const suitNames = ["Spades", "Clubs", "Diamond", "Hearts"]; // Declare suit names as constants
+const valueNames = ["Ace", "2","3","4","5","6","7","8","9","10","Jack", "Queen", "King"]; // Ditto with ranks
 
 function displayRandomDeck() {
-
     for (let i = 0; i < 52; i++) {
         let image = new Image();
         image.src = "../client/img/" + i + ".png";
@@ -27,7 +32,6 @@ function displayRandomDeck() {
             value: valueNames[i % 13]
         });
     }
-
 }
 
 function checkCards() {
@@ -38,28 +42,19 @@ function checkCards() {
     }
 }
 
-function shuffleCards() {
-
+function shuffleCards() { // Shuffles cards by swapping their values in the array
     for (let i = 0; i < 52; i++) {
         let j = Math.floor(Math.random() * 52);
         if (j === i) continue;
         deck.swap(i, j);
     }
-
 }
 
-function drawCards() {
-    let canvas = document.getElementById("cardCanvas");
-    let context = canvas.getContext("2d");
-
-    for (let i = 0; i < 52; i++) {
-
+function drawCards() { // Draws cards to the canvas element
+    for (let i = 0; i < 2; i++) {
         console.log(deck[i]);
-
         context.drawImage(deck[i].image, 0, 0, 691, 1056,(i % 13) * 70, Math.floor(i / 13) * 100, 64, 94);
-
     }
-
 }
 
 
